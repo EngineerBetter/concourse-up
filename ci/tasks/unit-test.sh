0eec58f066f8a98fb03ce30a9c2911dca34a7d58
Colin Simmons <colin.simmons@engineerbetter.com>
2017-04-11 14:09:05 +0100

    cleanup test

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-go get github.com/onsi/ginkgo/ginkgo github.com/onsi/gomega
 mkdir -p $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 mv concourse-up/* $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 cd $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
