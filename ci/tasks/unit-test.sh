b84502d0065d649c15a0c2668f79b416044a1837
Colin Simmons <colin.simmons@engineerbetter.com>
2017-04-11 13:14:18 +0100

    the perils of copy pasta

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -2,7 +2,7 @@
 
 go get github.com/onsi/ginkgo/ginkgo github.com/onsi/gomega
 mkdir -p $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
-mv sombrero-cli/* $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
+mv concourse-up/* $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 cd $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 ginkgo -r
 go run main.go
