97f53900791416df1ff94b714cee1f9c5eea90e1
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-19 10:15:14 +0100

    add helper function to ask for user confirmation

---
+++ util/check_confirmation.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+// CheckConfirmation promts the user for confirmation and returns true IFF the user responds with 'yes'
+func CheckConfirmation(stdin io.Reader, stdout io.Writer, name string) (bool, error) {
+	var response string
+	fmt.Fprintf(stdout, "Are you sure you want to destroy %s?\nThis cannot be undone. [yes/no]: ", name)
+
+	_, err := fmt.Fscan(stdin, &response)
+	if err != nil {
+		return false, err
+	}
+	response = strings.TrimSpace(response)
+	response = strings.ToLower(response)
+	if response == "yes" {
+		return true, nil
+	} else if response == "no" {
+		return false, nil
+	}
+
+	return false, fmt.Errorf("Input not recognized: `%s`", response)
+}
--- util/util_test.go
+++ util/util_test.go
@@ -1,6 +1,7 @@
 package util_test
 
 import (
+	"io"
 	"io/ioutil"
 	"os"
 	"os/user"
@@ -9,6 +10,7 @@ import (
 	"bitbucket.org/engineerbetter/concourse-up/util"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("util functions", func() {
@@ -156,6 +158,46 @@ var _ = Describe("util functions", func() {
 					contents, err := ioutil.ReadFile(path)
 					Ω(string(contents)).Should(Equal("hello world"))
 				})
+			})
+		})
+	})
+
+	Describe("confirmation check", func() {
+		var stdin io.ReadWriter
+		var stdout io.Writer
+
+		BeforeEach(func() {
+			stdin = gbytes.NewBuffer()
+			stdout = gbytes.NewBuffer()
+		})
+
+		Context("When the user responds with 'yes'", func() {
+			It("Returns true", func() {
+				stdin.Write([]byte("yes\n"))
+				returnVal, err := util.CheckConfirmation(stdin, stdout, "serrano")
+				Eventually(stdout).Should(gbytes.Say(`Are you sure you want to destroy serrano?`))
+				Eventually(returnVal).Should(Equal(true))
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("When the user responds with no", func() {
+			It("Returns false", func() {
+				stdin.Write([]byte("no\n"))
+				returnVal, err := util.CheckConfirmation(stdin, stdout, "serrano")
+				Eventually(stdout).Should(gbytes.Say(`Are you sure you want to destroy serrano?`))
+				Eventually(returnVal).Should(Equal(false))
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("When the user responds with any thing else", func() {
+			It("Returns a meaningful error message", func() {
+				stdin.Write([]byte("what\n"))
+				returnVal, err := util.CheckConfirmation(stdin, stdout, "serrano")
+				Eventually(stdout).Should(gbytes.Say(`Are you sure you want to destroy serrano?`))
+				Eventually(returnVal).Should(Equal(false))
+				Expect(err).To(MatchError("Input not recognized: `what`"))
 			})
 		})
 	})
