2af8e7626cafa1611fcb83d50dbe8b02982be1c7
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-19 16:39:23 +0100

    simplify system tests

--- ci/tasks/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dirname=$(dirname $0)
-. $dirname/../script_setup.sh
-
-aws ec2 delete-key-pair --key-name "cup-test-bosh"
--- ci/tasks/system-test.sh
+++ ci/tasks/system-test.sh
@@ -2,39 +2,13 @@
 
 set -eu
 
-dirname=$(dirname $0)
-. $dirname/../script_setup.sh
+go run main.go deploy system-test
 
-go install
+hasKey=$(aws ec2 describe-key-pairs | jq -r '.KeyPairs[] | select(.KeyName | contains("engineerbetter-concourseup-system-test")) | any')
 
-name=cup-test
-config_dir=".concourse-up"
-key_file="concourse-up-${name}.pem"
-key_file_path="${config_dir}/${key_file}"
+if [[ ! $hasKey == 'true' ]]; then
+  echo "Couldn't find key pair stating with engineerbetter-concourseup-system-test"
+  exit 1
+fi
 
-echo "Checking help command"
-concourse-up --help | grep -q "Concourse-Up - A CLI tool to deploy Concourse CI" || (echo "Failed to find description in help output" && exit 1)
-concourse-up --help | grep -q "deploy, d  Deploys or updates a Concourse" || (echo "Failed to find deploy instructions in help output" && exit 1)
-
-echo "Checking deploy command"
-concourse-up deploy --help | grep -q "Deploys or updates a Concourse" || (echo "Failed to display help output" && exit 1)
-concourse-up deploy | grep -q 'Usage is `concourse-up deploy <name>`' || (echo "Failed to show usage when name isnt' provided'" && exit 1)
-rm -rf ~/$config_dir
-! aws ec2 describe-key-pairs --key-name "${name}-bosh" || aws ec2 delete-key-pair --key-name "${name}-bosh"
-concourse-up deploy $name || (echo "concourse-up deploy failed" && exit 1)
-pushd ~/
-  [[ -d $config_dir ]] || (echo "$config_dir not created" && exit 1)
-  ls -la $key_file_path || (echo "$key_file not created" && exit 1)
-  grep -q "BEGIN RSA PRIVATE KEY" $key_file_path || (echo "private_key not written to config file" && exit 1)
-  config_md5=$(md5sum $key_file_path | awk {'print $1'})
-popd
-aws ec2 describe-key-pairs --key-name "${name}-bosh" || (echo "keypair ${name}-bosh not found in AWS" && exit 1)
-concourse-up deploy $name || (echo "second concourse-up deploy failed" && exit 1)
-pushd ~/
-  config2_md5=$(md5sum $key_file_path | awk {'print $1'})
-popd
-diff  <(echo "$config_md5" ) <(echo "$config2_md5") &>/dev/null || (echo "local key was changed" && exit 1)
-
-# TEMP delete until destroy command is implemented
-aws ec2 delete-key-pair --key-name "${name}-bosh"
-rm -rf ~/$config_dir
+# concourse-up destroy system-test
