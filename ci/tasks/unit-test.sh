d78e28bc3783041a763bbd12211ee996ba3693ec
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-19 16:46:12 +0100

    ensure concourse-up is in go-path for unit tests

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -2,8 +2,4 @@
 
 set -eu
 
-mkdir -p $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
-mv concourse-up/* $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
-cd $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
-
 ginkgo -r
