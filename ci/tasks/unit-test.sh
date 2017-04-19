613f26ea1e7b81f0cdeb5a2ff2db75aa86fed4f3
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-19 16:23:32 +0100

    merge image building and testing pipelines

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -6,7 +6,4 @@ mkdir -p $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 mv concourse-up/* $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 cd $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 
-go get github.com/onsi/ginkgo/ginkgo github.com/onsi/gomega
-
 ginkgo -r
-go run main.go
