b8c9448e83eedfe1d1fd096ed4fd879e056eda78
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-19 18:15:26 +0100

    allow system tests to run concurrently

--- ci/tasks/system-test.sh
+++ ci/tasks/system-test.sh
@@ -5,13 +5,19 @@ set -eu
 export GOPATH=$PWD/go
 cd go/src/bitbucket.org/engineerbetter/concourse-up
 
-go run main.go deploy system-test
+deployment="engineerbetter-concourseup-system-test-$RANDOM"
 
-hasKey=$(aws ec2 describe-key-pairs | jq -r '.KeyPairs[] | select(.KeyName | contains("engineerbetter-concourseup-system-test")) | any')
+go run main.go deploy $deployment
+
+hasKey=$(aws ec2 describe-key-pairs | jq -r ".KeyPairs[] | select(.KeyName | contains(\"$deployment\")) | any")
 
 if [[ ! $hasKey == 'true' ]]; then
-  echo "Couldn't find key pair stating with engineerbetter-concourseup-system-test"
+  echo "Couldn't find key pair stating with $deployment"
   exit 1
 fi
 
-# concourse-up destroy system-test
+concourse-up --non-interactive destroy $deployment
+
+aws s3api delete-objects --bucket $deployment --delete "$(aws s3api list-object-versions --bucket $deployment | jq -M '{Objects: [.["Versions","DeleteMarkers"][]| {Key:.Key, VersionId : .VersionId}], Quiet: false}')"
+
+aws s3 rb s3://$deployment --force
