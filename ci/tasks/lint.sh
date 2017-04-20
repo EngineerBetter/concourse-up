97ec8c47f43ce37096f0a1ea8c08db488a2c9643
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-20 10:13:54 +0100

    remove useless linters

--- ci/tasks/lint.sh
+++ ci/tasks/lint.sh
@@ -6,4 +6,9 @@ mkdir -p $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 mv concourse-up/* $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 cd $GOPATH/src/bitbucket.org/engineerbetter/concourse-up
 
-gometalinter.v1 -e vendor -e "_test\.go" --deadline=120s ./...
+gometalinter.v1 --exclude vendor \
+  --exclude "_test\.go" \
+  --disable=gotype \
+  --disable=gas \
+  --deadline=120s \
+  ./...
