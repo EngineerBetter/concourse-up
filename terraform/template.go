7364f329a46087b967bf0889479cd1483892e975
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-24 18:09:04 +0100

    add public ip to all vms since no NAT box

--- terraform/template.go
+++ terraform/template.go
@@ -177,7 +177,7 @@ resource "aws_subnet" "default" {
   vpc_id                  = "${aws_vpc.default.id}"
   availability_zone       = "${var.availability_zone}"
   cidr_block              = "10.0.0.0/24"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags {
     Name = "${var.deployment}"
