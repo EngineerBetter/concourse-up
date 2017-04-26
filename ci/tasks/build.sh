2c724f01efb6bb181e75afe2147b92c39da7304b
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-26 13:58:59 +0100

    ensure build artifacts are saved to correct dir

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,10 +2,13 @@
 
 set -eu
 
+build_dir=$PWD/build
+mkdir -p build_dir
+
 mkdir -p $GOPATH/src/github.com/engineerbetter/concourse-up
 mv concourse-up/* $GOPATH/src/github.com/engineerbetter/concourse-up
 cd $GOPATH/src/github.com/engineerbetter/concourse-up
 
-GOOS=linux GOARCH=amd64 go build -o build/concourse-up-linux-amd64
-GOOS=darwin GOARCH=amd64 go build -o build/concourse-up-darwin-amd64
-GOOS=windows GOARCH=amd64 go build -o build/concourse-up-windows-amd64.exe
+GOOS=linux GOARCH=amd64 go build -o $build_dir/concourse-up-linux-amd64
+GOOS=darwin GOARCH=amd64 go build -o $build_dir/concourse-up-darwin-amd64
+GOOS=windows GOARCH=amd64 go build -o $build_dir/concourse-up-windows-amd64.exe
