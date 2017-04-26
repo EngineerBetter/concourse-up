875231aa5a34f914126ef4f715cf0ef90859e315
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-26 15:01:14 +0100

    make version a variable that can be updated at compile time

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
+var concourseUpVersion = "release-candidate"
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "Concourse-Up"
 	app.Usage = "A CLI tool to deploy Concourse CI"
+	app.Version = concourseUpVersion
 	app.Commands = commands.Commands
 	app.Flags = commands.GlobalFlags
 	if err := app.Run(os.Args); err != nil {
