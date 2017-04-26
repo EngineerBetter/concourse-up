ef2b7ae3a3e2b2d20b84921876f3eed9ed1d5bb6
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-26 17:10:25 +0100

    add system test for non custom domain

--- ci/tasks/system-test.sh
+++ ci/tasks/system-test.sh
@@ -7,13 +7,25 @@ mv concourse-up/* $GOPATH/src/github.com/engineerbetter/concourse-up
 cd $GOPATH/src/github.com/engineerbetter/concourse-up
 
 deployment="system-test-$RANDOM"
-domain="$deployment.concourse-up.engineerbetter.com"
 
-go run main.go deploy $deployment --domain $domain
+if [[ -n $ROOT_DOMAIN ]]; then
+  domain="$deployment.$ROOT_DOMAIN"
 
-config=$(go run main.go info $deployment)
-username=$(echo $config | jq -r '.config.concourse_username')
-password=$(echo $config | jq -r '.config.concourse_password')
+  go run main.go deploy $deployment --domain $domain
+
+  config=$(go run main.go info $deployment)
+
+  username=$(echo $config | jq -r '.config.concourse_username')
+  password=$(echo $config | jq -r '.config.concourse_password')
+else
+  go run main.go deploy $deployment
+
+  config=$(go run main.go info $deployment)
+
+  domain=$(echo $config | jq -r '.terraform.elb_dns_name.value')
+  username=$(echo $config | jq -r '.config.concourse_username')
+  password=$(echo $config | jq -r '.config.concourse_password')
+fi
 
 fly --target system-test login --insecure --concourse-url https://$domain --username $username --password $password
 fly --target system-test workers
