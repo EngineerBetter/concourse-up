c06435bd2cdc7787367f531f75f5c87874978586
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-26 17:25:26 +0100

    check custom domain and no custom domain in same system test

--- ci/tasks/system-test.sh
+++ ci/tasks/system-test.sh
@@ -8,26 +8,32 @@ cd $GOPATH/src/github.com/engineerbetter/concourse-up
 
 deployment="system-test-$RANDOM"
 
-if [[ -n $ROOT_DOMAIN ]]; then
-  domain="$deployment.$ROOT_DOMAIN"
+# CASE 1) NO CUSTOM DOMAIN
+go run main.go deploy $deployment
 
-  go run main.go deploy $deployment --domain $domain
+config=$(go run main.go info $deployment)
 
-  config=$(go run main.go info $deployment)
-
-  username=$(echo $config | jq -r '.config.concourse_username')
-  password=$(echo $config | jq -r '.config.concourse_password')
-else
-  go run main.go deploy $deployment
-
-  config=$(go run main.go info $deployment)
-
-  domain=$(echo $config | jq -r '.terraform.elb_dns_name.value')
-  username=$(echo $config | jq -r '.config.concourse_username')
-  password=$(echo $config | jq -r '.config.concourse_password')
-fi
+domain=$(echo $config | jq -r '.terraform.elb_dns_name.value')
+username=$(echo $config | jq -r '.config.concourse_username')
+password=$(echo $config | jq -r '.config.concourse_password')
 
 fly --target system-test login --insecure --concourse-url https://$domain --username $username --password $password
 fly --target system-test workers
+
+# CASE 2) CUSTOM DOMAIN, NO PROVIDED CERT
+
+domain="$deployment.$ROOT_DOMAIN"
+
+go run main.go deploy $deployment --domain $domain
+
+config=$(go run main.go info $deployment)
+
+username=$(echo $config | jq -r '.config.concourse_username')
+password=$(echo $config | jq -r '.config.concourse_password')
+
+fly --target system-test login --insecure --concourse-url https://$domain --username $username --password $password
+fly --target system-test workers
+
+# DESTROY
 
 go run main.go --non-interactive destroy $deployment
