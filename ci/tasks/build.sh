2dc531587fef1e999eebb6e868476a13e3f7b254
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-26 15:02:37 +0100

    ensure binary is compiled with correct version

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -5,10 +5,12 @@ set -eu
 build_dir=$PWD/build
 mkdir -p build_dir
 
+version=$(cat version/version)
+
 mkdir -p $GOPATH/src/github.com/engineerbetter/concourse-up
 mv concourse-up/* $GOPATH/src/github.com/engineerbetter/concourse-up
 cd $GOPATH/src/github.com/engineerbetter/concourse-up
 
-GOOS=linux GOARCH=amd64 go build -o $build_dir/concourse-up-linux-amd64
-GOOS=darwin GOARCH=amd64 go build -o $build_dir/concourse-up-darwin-amd64
-GOOS=windows GOARCH=amd64 go build -o $build_dir/concourse-up-windows-amd64.exe
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.concourseUpVersion=$version" -o $build_dir/concourse-up-linux-amd64
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.concourseUpVersion=$version" -o $build_dir/concourse-up-darwin-amd64
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.concourseUpVersion=$version" -o $build_dir/concourse-up-windows-amd64.exe
