48a0d6e8b1dba11ee19645d667539db76893693b
Peter Ellis Jones <pete@peterellisjones.com>
2017-04-28 12:37:29 +0100

    export more variables in compile releases script

--- ci/tasks/compile-bosh-releases.sh
+++ ci/tasks/compile-bosh-releases.sh
@@ -6,7 +6,22 @@ echo "$BOSH_CA_CERT" > bosh_ca_cert.pem
 
 bosh_flags="--non-interactive --environment $BOSH_TARGET --client $BOSH_USERNAME --client-secret $BOSH_PASSWORD --ca-cert bosh_ca_cert.pem"
 
-stemcell_version=$(cat concourse-stemcell/version)
+concourse_stemcell_version=$(cat concourse-stemcell/version)
+concourse_stemcell_url=$(cat concourse-stemcell/url)
+concourse_stemcell_sha1=$(cat concourse-stemcell/sha1)
+
+director_stemcell_version=$(cat director-stemcell/version)
+director_stemcell_url=$(cat director-stemcell/url)
+director_stemcell_sha1=$(cat director-stemcell/sha1)
+
+director_bosh_release_version=$(cat director-bosh-release/version)
+director_bosh_release_url=$(cat director-bosh-release/url)
+director_bosh_release_sha1=$(cat director-bosh-release/sha1)
+
+director_bosh_cpi_release_version=$(cat director-bosh-cpi-release/version)
+director_bosh_cpi_release_url=$(cat director-bosh-cpi-release/url)
+director_bosh_cpi_release_sha1=$(cat director-bosh-cpi-release/sha1)
+
 concourse_release_version=$(ls concourse-bosh-release/concourse-*.tgz | awk -F"-" '{ print $4 }' | awk -F".tgz" '{ print $1 }')
 garden_release_version=$(ls concourse-bosh-release/garden-runc-*.tgz | awk -F"-" '{ print $5 }' | awk -F".tgz" '{ print $1 }')
 
@@ -19,14 +34,14 @@ name: concourse-empty
 
 releases:
 - name: concourse
-  version: $concourse_release_version
+  version: \"$concourse_release_version\"
 - name: garden-runc
-  version: $garden_release_version
+  version: \"$garden_release_version\"
 
 stemcells:
 - alias: trusty
   os: ubuntu-trusty
-  version: $stemcell_version
+  version: \"$concourse_stemcell_version\"
 
 update:
   canaries: 1
@@ -42,25 +57,38 @@ bosh-cli $bosh_flags \
 
 bosh-cli $bosh_flags \
   --deployment concourse-empty \
-  export-release concourse/$concourse_release_version ubuntu-trusty/$stemcell_version
+  export-release concourse/$concourse_release_version ubuntu-trusty/$concourse_stemcell_version
 
 bosh-cli $bosh_flags \
   --deployment concourse-empty \
-  export-release garden-runc/$garden_release_version ubuntu-trusty/$stemcell_version
+  export-release garden-runc/$garden_release_version ubuntu-trusty/$concourse_stemcell_version
 
-compiled_concourse_release=$(ls concourse-$concourse_release_version-ubuntu-trusty-$stemcell_version-*.tgz)
-compiled_garden_release=$(ls garden-runc-$garden_release_version-ubuntu-trusty-$stemcell_version-*.tgz)
+compiled_concourse_release=$(ls concourse-$concourse_release_version-ubuntu-trusty-$concourse_stemcell_version-*.tgz)
+compiled_garden_release=$(ls garden-runc-$garden_release_version-ubuntu-trusty-$concourse_stemcell_version-*.tgz)
 
 aws s3 cp --acl public-read $compiled_concourse_release s3://$PUBLIC_ARTIFACTS_BUCKET/$compiled_concourse_release
 aws s3 cp --acl public-read $compiled_garden_release s3://$PUBLIC_ARTIFACTS_BUCKET/$compiled_garden_release
 
-stemcell_url=$(cat concourse-stemcell/url)
-
 echo "{
-  \"stemcell_url\": \"$stemcell_url\",
+  \"concourse_stemcell_url\": \"$concourse_stemcell_url\",
+  \"concourse_stemcell_sha1\": \"$concourse_stemcell_sha1\",
+  \"concourse_stemcell_version\": \"$concourse_stemcell_version\",
+
+  \"director_stemcell_url\": \"$director_stemcell_url\",
+  \"director_stemcell_sha1\": \"$director_stemcell_sha1\",
+  \"director_stemcell_version\": \"$director_stemcell_version\",
+
+  \"director_bosh_release_url\": \"$director_bosh_release_url\",
+  \"director_bosh_release_sha1\": \"$director_bosh_release_sha1\",
+  \"director_bosh_release_version\": \"$director_bosh_release_version\",
+
+  \"director_bosh_cpi_release_url\": \"$director_bosh_cpi_release_url\",
+  \"director_bosh_cpi_release_sha1\": \"$director_bosh_cpi_release_sha1\",
+  \"director_bosh_cpi_release_version\": \"$director_bosh_cpi_release_version\",
+
   \"concourse_release_url\": \"https://s3-$AWS_DEFAULT_REGION.amazonaws.com/$PUBLIC_ARTIFACTS_BUCKET/$compiled_concourse_release\",
-  \"garden_release_url\": \"https://s3-$AWS_DEFAULT_REGION.amazonaws.com/$PUBLIC_ARTIFACTS_BUCKET/$compiled_garden_release\",
-  \"stemcell_version\": \"$stemcell_version\",
   \"concourse_release_version\": \"concourse_release_version\",
+
+  \"garden_release_url\": \"https://s3-$AWS_DEFAULT_REGION.amazonaws.com/$PUBLIC_ARTIFACTS_BUCKET/$compiled_garden_release\",
   \"garden_release_version\": \"garden_release_version\"
 }" > compilation-vars/compilation-vars.json
