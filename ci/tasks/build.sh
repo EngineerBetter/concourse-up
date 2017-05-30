0caabd6b2ac832b52334fd309e96b2f5a4ace477
Daniel Jones <daniel.jones@engineerbetter.com>
2017-05-30 08:57:47 +0100

    update CI build script for camel-cased package

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -23,22 +23,22 @@ concourse_release_version=$(cat compilation-vars/compilation-vars.json | jq -r .
 garden_release_url=$(cat compilation-vars/compilation-vars.json | jq -r .garden_release_url)
 garden_release_version=$(cat compilation-vars/compilation-vars.json | jq -r .garden_release_version)
 
-mkdir -p $GOPATH/src/github.com/engineerbetter/concourse-up
-mv concourse-up/* $GOPATH/src/github.com/engineerbetter/concourse-up
-cd $GOPATH/src/github.com/engineerbetter/concourse-up
+mkdir -p $GOPATH/src/github.com/EngineerBetter/concourse-up
+mv concourse-up/* $GOPATH/src/github.com/EngineerBetter/concourse-up
+cd $GOPATH/src/github.com/EngineerBetter/concourse-up
 
 go build -ldflags "
   -X main.concourseUpVersion=$version
-  -X github.com/engineerbetter/concourse-up/bosh.concourseStemcellURL=$concourse_stemcell_url
-  -X github.com/engineerbetter/concourse-up/bosh.concourseStemcellVersion=$concourse_stemcell_version
-  -X github.com/engineerbetter/concourse-up/bosh.concourseCompiledReleaseURL=$concourse_release_url
-  -X github.com/engineerbetter/concourse-up/bosh.concourseReleaseVersion=$concourse_release_version
-  -X github.com/engineerbetter/concourse-up/bosh.gardenCompiledReleaseURL=$garden_release_url
-  -X github.com/engineerbetter/concourse-up/bosh.gardenReleaseVersion=$garden_release_version
-  -X github.com/engineerbetter/concourse-up/bosh.directorStemcellURL=$director_stemcell_url
-  -X github.com/engineerbetter/concourse-up/bosh.directorStemcellSHA1=$director_stemcell_sha1
-  -X github.com/engineerbetter/concourse-up/bosh.directorCPIReleaseURL=$director_bosh_cpi_release_url
-  -X github.com/engineerbetter/concourse-up/bosh.directorCPIReleaseSHA1=$director_bosh_cpi_release_sha1
-  -X github.com/engineerbetter/concourse-up/bosh.directorReleaseURL=$director_bosh_release_url
-  -X github.com/engineerbetter/concourse-up/bosh.directorReleaseSHA1=$director_bosh_release_sha1
+  -X github.com/EngineerBetter/concourse-up/bosh.concourseStemcellURL=$concourse_stemcell_url
+  -X github.com/EngineerBetter/concourse-up/bosh.concourseStemcellVersion=$concourse_stemcell_version
+  -X github.com/EngineerBetter/concourse-up/bosh.concourseCompiledReleaseURL=$concourse_release_url
+  -X github.com/EngineerBetter/concourse-up/bosh.concourseReleaseVersion=$concourse_release_version
+  -X github.com/EngineerBetter/concourse-up/bosh.gardenCompiledReleaseURL=$garden_release_url
+  -X github.com/EngineerBetter/concourse-up/bosh.gardenReleaseVersion=$garden_release_version
+  -X github.com/EngineerBetter/concourse-up/bosh.directorStemcellURL=$director_stemcell_url
+  -X github.com/EngineerBetter/concourse-up/bosh.directorStemcellSHA1=$director_stemcell_sha1
+  -X github.com/EngineerBetter/concourse-up/bosh.directorCPIReleaseURL=$director_bosh_cpi_release_url
+  -X github.com/EngineerBetter/concourse-up/bosh.directorCPIReleaseSHA1=$director_bosh_cpi_release_sha1
+  -X github.com/EngineerBetter/concourse-up/bosh.directorReleaseURL=$director_bosh_release_url
+  -X github.com/EngineerBetter/concourse-up/bosh.directorReleaseSHA1=$director_bosh_release_sha1
 " -o $build_dir/$OUTPUT_FILE
