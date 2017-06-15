eb6850f3c2c63c662550ac774dab55379db97730
Peter Ellis Jones <pete@peterellisjones.com>
2017-06-15 11:16:46 -0700

    dont compile aws cpi release

--- ci/tasks/compile-bosh-releases.sh
+++ ci/tasks/compile-bosh-releases.sh
@@ -14,8 +14,11 @@ director_stemcell_version=$(cat director-stemcell/version)
 director_stemcell_url=$(cat director-stemcell/url)
 director_stemcell_sha1=$(cat director-stemcell/sha1)
 
-director_bosh_release_version=$(cat director-bosh-release/version)
 director_bosh_cpi_release_version=$(cat director-bosh-cpi-release/version)
+director_bosh_cpi_release_url=$(cat director-bosh-cpi-release/url)
+director_bosh_cpi_release_sha1=$(cat director-bosh-cpi-release/sha1)
+
+director_bosh_release_version=$(cat director-bosh-release/version)
 concourse_release_version=$(ls concourse-bosh-release/concourse-*.tgz | awk -F"-" '{ print $4 }' | awk -F".tgz" '{ print $1 }')
 garden_release_version=$(ls concourse-bosh-release/garden-runc-*.tgz | awk -F"-" '{ print $5 }' | awk -F".tgz" '{ print $1 }')
 
@@ -23,7 +26,6 @@ bosh-cli $bosh_flags upload-stemcell "concourse-stemcell/stemcell.tgz"
 bosh-cli $bosh_flags upload-release "concourse-bosh-release/garden-runc-$garden_release_version.tgz"
 bosh-cli $bosh_flags upload-release "concourse-bosh-release/concourse-$concourse_release_version.tgz"
 bosh-cli $bosh_flags upload-release "director-bosh-release/release.tgz"
-bosh-cli $bosh_flags upload-release "director-bosh-cpi-release/release.tgz"
 
 echo "---
 name: concourse-empty
@@ -35,8 +37,6 @@ releases:
   version: \"$garden_release_version\"
 - name: bosh
   version: \"$director_bosh_release_version\"
-- name: bosh-aws-cpi
-  version: \"$director_bosh_cpi_release_version\"
 
 stemcells:
 - alias: trusty
@@ -67,22 +67,15 @@ bosh-cli $bosh_flags \
   --deployment concourse-empty \
   export-release bosh/$director_bosh_release_version ubuntu-trusty/$concourse_stemcell_version
 
-bosh-cli $bosh_flags \
-  --deployment concourse-empty \
-  export-release bosh-aws-cpi/$director_bosh_cpi_release_version ubuntu-trusty/$concourse_stemcell_version
-
 compiled_concourse_release=$(ls concourse-$concourse_release_version-ubuntu-trusty-$concourse_stemcell_version-*.tgz)
 compiled_garden_release=$(ls garden-runc-$garden_release_version-ubuntu-trusty-$concourse_stemcell_version-*.tgz)
 compiled_director_bosh_release=$(ls bosh-$director_bosh_release_version-ubuntu-trusty-$concourse_stemcell_version-*.tgz)
-compiled_director_bosh_cpi_release=$(ls bosh-aws-cpi-$director_bosh_cpi_release_version-ubuntu-trusty-$concourse_stemcell_version-*.tgz)
 
 aws s3 cp --acl public-read "$compiled_concourse_release" "s3://$PUBLIC_ARTIFACTS_BUCKET/$compiled_concourse_release"
 aws s3 cp --acl public-read "$compiled_garden_release" "s3://$PUBLIC_ARTIFACTS_BUCKET/$compiled_garden_release"
 aws s3 cp --acl public-read "$compiled_director_bosh_release" "s3://$PUBLIC_ARTIFACTS_BUCKET/$compiled_director_bosh_release"
-aws s3 cp --acl public-read "$compiled_director_bosh_cpi_release" "s3://$PUBLIC_ARTIFACTS_BUCKET/$compiled_director_bosh_cpi_release"
 
 director_bosh_release_sha1=$(sha1sum "$compiled_director_bosh_release" | awk '{ print $1 }')
-director_bosh_cpi_release_sha1=$(sha1sum "$compiled_director_bosh_cpi_release" | awk '{ print $1 }')
 concourse_release_sha1=$(sha1sum "$compiled_concourse_release" | awk '{ print $1 }')
 garden_release_sha1=$(sha1sum "$compiled_garden_release" | awk '{ print $1 }')
 
@@ -99,7 +92,7 @@ echo "{
   \"director_bosh_release_sha1\": \"$director_bosh_release_sha1\",
   \"director_bosh_release_version\": \"$director_bosh_release_version\",
 
-  \"director_bosh_cpi_release_url\": \"https://s3-$AWS_DEFAULT_REGION.amazonaws.com/$PUBLIC_ARTIFACTS_BUCKET/$compiled_director_bosh_cpi_release\",
+  \"director_bosh_cpi_release_url\": \"$director_bosh_cpi_release_url\",
   \"director_bosh_cpi_release_sha1\": \"$director_bosh_cpi_release_sha1\",
   \"director_bosh_cpi_release_version\": \"$director_bosh_cpi_release_version\",
 
