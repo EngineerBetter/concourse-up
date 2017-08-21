c917d903f8d82ad51494a70ada1e0d8603ea278f
Peter Jones <pete@peterellisjones.com>
2017-08-21 12:35:25 +0100

    put version in commit message

--- ci/tasks/copy-compilation-vars.sh
+++ ci/tasks/copy-compilation-vars.sh
@@ -4,7 +4,11 @@ set -eu
 
 cp -r concourse-up/. concourse-up-new
 cp compilation-vars/compilation-vars.json concourse-up-new/compilation-vars.json
+version=$(cat version/version)
 
 pushd concourse-up-new
   git add compilation-vars.json
+  git config --global user.email "systems@engineerbetter.com"
+  git config --global user.name "CI"
+  git commit -m "add compilation-vars.json for version $version"
 popd
