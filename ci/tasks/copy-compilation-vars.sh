60dd32655e99f3b673a4a046f9b5983b93ea4573
Peter Jones <pete@peterellisjones.com>
2017-08-21 12:30:35 +0100

    fix script to commit compilation vars

--- ci/tasks/copy-compilation-vars.sh
+++ ci/tasks/copy-compilation-vars.sh
@@ -2,10 +2,9 @@
 
 set -eu
 
-cp -R concourse-up/* concourse-up-new
+cp -r concourse-up/. concourse-up-new
 cp compilation-vars/compilation-vars.json concourse-up-new/compilation-vars.json
 
-pushd
-  concourse-up-new
+pushd concourse-up-new
   git add compilation-vars.json
 popd
