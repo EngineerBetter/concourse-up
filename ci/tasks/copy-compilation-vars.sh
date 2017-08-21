f0c6932e26fba8db5a78b771c909ff1c65c55a1b
Peter Jones <pete@peterellisjones.com>
2017-08-21 12:01:14 +0100

    ensure that repo is put in outdir before committing

--- ci/tasks/copy-compilation-vars.sh
+++ ci/tasks/copy-compilation-vars.sh
@@ -2,4 +2,10 @@
 
 set -eu
 
-cp compilation-vars/compilation-vars.json concourse-up/compilation-vars.json
+cp -R concourse-up/* concourse-up-new
+cp compilation-vars/compilation-vars.json concourse-up-new/compilation-vars.json
+
+pushd
+  concourse-up-new
+  git add compilation-vars.json
+popd
