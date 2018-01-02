ca62f950180f1e2f1a965bbcfd08e9cd1bcb385a
Paddy Steed <paddy.steed@engineerbetter.com>
2018-01-02 16:49:21 +0000

    Replace broken self-update-test with update-test
    The new test upgrades the most recently released version with the new version then runs acceptance tests.
    
    This should prevent a bad release like 0.7.1.

---
+++ ci/tasks/system-test-update.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# We can't test that concourse-up will update itself to a latest release without publishing a new release
+# Instead we will test that if we publish a non-existant release, the self-update will revert back to a known release
+
+[ "$VERBOSE" ] && { set -x; export BOSH_LOG_LEVEL=debug; }
+set -eu
+
+deployment="system-test-$RANDOM"
+
+cp release/concourse-up-linux-amd64 ./cup-old
+cp "$BINARY_PATH" ./cup-new
+chmod +x ./cup-*
+
+echo "DEPLOY OLD VERSION"
+
+./cup-old deploy $deployment
+
+echo "UPDATE TO NEW VERSION"
+
+./cup-new deploy $deployment
+
+sleep 60
+
+config=$(./cup info --region us-east-1 --json $deployment)
+domain=$(echo "$config" | jq -r '.config.domain')
+username=$(echo "$config" | jq -r '.config.concourse_username')
+password=$(echo "$config" | jq -r '.config.concourse_password')
+echo "$config" | jq -r '.config.concourse_ca_cert' > generated-ca-cert.pem
+
+fly --target system-test login \
+  --ca-cert generated-ca-cert.pem \
+  --concourse-url "https://$domain" \
+  --username "$username" \
+  --password "$password"
+
+curl -k "https://$domain:3000"
+
+fly --target system-test sync
+
+fly --target system-test set-pipeline \
+  --non-interactive \
+  --pipeline hello \
+  --config "$(dirname "$0")/hello.yml"
+
+fly --target system-test unpause-pipeline \
+    --pipeline hello
+
+fly --target system-test trigger-job \
+  --job hello/hello \
+  --watch
+
+echo "DESTROYING DEPLOYMENT"
+./cup-new --non-interactive destroy $deployment
