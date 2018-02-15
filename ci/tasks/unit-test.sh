65faa27f4f8559f6b10cd0cf9baf3d2c2d1b1e24
Paddy Steed <paddy.steed@engineerbetter.com>
2018-02-15 18:40:02 +0000

    Fix unit tests

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -6,5 +6,8 @@ mkdir -p "$GOPATH/src/github.com/EngineerBetter/concourse-up"
 mv concourse-up/* "$GOPATH/src/github.com/EngineerBetter/concourse-up"
 cd "$GOPATH/src/github.com/EngineerBetter/concourse-up"
 
+go get -u github.com/mattn/go-bindata/...
+go generate github.com/EngineerBetter/concourse-up/bosh
+go generate github.com/EngineerBetter/concourse-up/terraform
 export CONCOURSE_UP_ACME_URL=https://acme-staging.api.letsencrypt.org/directory # Avoid rate limits when testing
 ginkgo -r
