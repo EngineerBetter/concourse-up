0399bc192e4c13d68516cb86386ecde70a774dc4
Paddy Steed <paddy.steed@engineerbetter.com>
2018-03-07 10:22:14 +0000

    Bump RDS postgresql version

--- terraform/assets/main.tf
+++ terraform/assets/main.tf
@@ -535,7 +535,7 @@ resource "aws_db_instance" "default" {
   port                   = 5432
   engine                 = "postgres"
   instance_class         = "${var.rds_instance_class}"
-  engine_version         = "9.6.1"
+  engine_version         = "9.6.6"
   name                   = "${var.rds_default_database_name}"
   username               = "${var.rds_instance_username}"
   password               = "${var.rds_instance_password}"
