4b4f9bdb6bc0b71cbd29788588e216f8a010fa27
Paddy Steed <paddy.steed@engineerbetter.com>
2018-03-09 09:49:41 +0000

    Allow expanding RDS storage

--- terraform/assets/main.tf
+++ terraform/assets/main.tf
@@ -544,7 +544,9 @@ resource "aws_db_instance" "default" {
   vpc_security_group_ids = ["${aws_security_group.rds.id}"]
   db_subnet_group_name   = "${aws_db_subnet_group.default.name}"
   skip_final_snapshot    = true
-
+  lifecycle {
+    ignore_changes = ["allocated_storage"]
+  }
   tags {
     Name = "${var.deployment}"
     concourse-up-project = "${var.project}"
