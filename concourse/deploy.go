71d036bedf62cbe591af9a578b447a75445fcc53
Colin Simmons <colin.simmons@engineerbetter.com>
2018-04-17 13:46:37 +0100

    put project name on correct side of info in command

--- concourse/deploy.go
+++ concourse/deploy.go
@@ -373,7 +373,7 @@ fly --target {{.Project}} login{{if not .ConcourseUserProvidedCert}} --insecure{
 Metrics available at https://{{.Domain}}:3000 using the same username and password
 
 Log into credhub with:
-eval "$(concourse-up {{.Project}} info --env --region {{.Region}})"
+eval "$(concourse-up info {{.Project}} --env --region {{.Region}})"
 `
 
 func writeDeploySuccessMessage(config *config.Config, metadata *terraform.Metadata, stdout io.Writer) error {
