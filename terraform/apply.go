589e2291f57e08af3c9e7b14b940a3008a1e0bb3
Will Gant <will.gant@engineerbetter.com>
2018-05-15 12:36:20 +0100

    add terraform debugging
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- terraform/apply.go
+++ terraform/apply.go
@@ -1,16 +1,20 @@
 package terraform
 
 import (
+	"fmt"
 	"io"
+	"os"
 	"os/exec"
 )
 
 // Apply takes a terraform config and applies it
 func (client *Client) Apply(dryrun bool) error {
+	fmt.Println("RUNNING TERRAFORM APPLY")
 	action := "apply"
 	if dryrun {
 		action = "plan"
 	}
+	os.Setenv("TF_LOG", "TRACE")
 	return client.terraform([]string{
 		action,
 		"-input=false",
