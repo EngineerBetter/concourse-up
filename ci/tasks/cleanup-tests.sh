5459443783fbf72f03b8737029327046219e4d33
Will Gant <will.gant@engineerbetter.com>
2018-05-21 10:35:18 +0100

    get concourse-up binary in cleanup job
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- ci/tasks/cleanup-tests.sh
+++ ci/tasks/cleanup-tests.sh
@@ -3,7 +3,10 @@
 set -e
 [ "$VERBOSE" ] && { set -x; export BOSH_LOG_LEVEL=debug; export BOSH_LOG_PATH=bosh.log; }
 
+cp release/concourse-up-linux-amd64 ./concourse-up
+chmod +x ./concourse-up
+
 aws s3 ls \
-| awk -F- '/concourse-up-system-test/{print "yes yes | concourse-up destroy --region "$8"-"$9"-"$10" "$5"-"$6"-"$7}' \
+| awk -F- '/concourse-up-system-test/{print "yes yes | ./concourse-up destroy --region "$8"-"$9"-"$10" "$5"-"$6"-"$7}' \
 | sort -u \
 | xargs -P 8 -I {} bash -c '{}'
