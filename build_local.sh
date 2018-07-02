c6af16c54fe8c753ee79a4aca8e1b2489ed0b989
Daniel Jones <daniel.jones@engineerbetter.com>
2018-07-02 19:07:49 +0100

    it helps to pass build vars in

--- build_local.sh
+++ build_local.sh
@@ -9,6 +9,8 @@ concourse_stemcell_version=$(jq -r .concourse_stemcell_version compilation-vars.
 director_stemcell_url=$(jq -r .director_stemcell_url compilation-vars.json)
 director_stemcell_sha1=$(jq -r .director_stemcell_sha1 compilation-vars.json)
 director_stemcell_version=$(jq -r .director_stemcell_version compilation-vars.json)
+director_bpm_release_url=$(jq -r .director_bpm_release_url compilation-vars.json)
+director_bpm_release_sha1=$(jq -r .director_bpm_release_sha1 compilation-vars.json)
 director_bosh_release_url=$(jq -r .director_bosh_release_url compilation-vars.json)
 director_bosh_release_version=$(jq -r .director_bosh_release_version compilation-vars.json)
 director_bosh_release_sha1=$(jq -r .director_bosh_release_sha1 compilation-vars.json)
@@ -76,6 +78,8 @@ go build -ldflags "
   -X github.com/EngineerBetter/concourse-up/bosh.DirectorStemcellURL=$director_stemcell_url
   -X github.com/EngineerBetter/concourse-up/bosh.DirectorStemcellSHA1=$director_stemcell_sha1
   -X github.com/EngineerBetter/concourse-up/bosh.DirectorStemcellVersion=$director_stemcell_version
+  -X github.com/EngineerBetter/concourse-up/bosh.DirectorBPMReleaseURL=$director_bpm_release_url
+  -X github.com/EngineerBetter/concourse-up/bosh.DirectorBPMReleaseSHA1=$director_bpm_release_sha1
   -X github.com/EngineerBetter/concourse-up/bosh.DirectorCPIReleaseURL=$director_bosh_cpi_release_url
   -X github.com/EngineerBetter/concourse-up/bosh.DirectorCPIReleaseVersion=$director_bosh_cpi_release_version
   -X github.com/EngineerBetter/concourse-up/bosh.DirectorCPIReleaseSHA1=$director_bosh_cpi_release_sha1
