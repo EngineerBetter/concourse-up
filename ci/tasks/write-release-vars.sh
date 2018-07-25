fb3003d55a91875308c4f21ecd3e5aa6f1c7a050
Sapphire Mason-Brown <sapphire.mason-brown@engineerbetter.com>
2018-07-25 12:49:35 +0100

    Extract versions from versioned release files
    Signed-off-by: Paddy Steed <paddy.steed@engineerbetter.com>

--- ci/tasks/write-release-vars.sh
+++ ci/tasks/write-release-vars.sh
@@ -2,34 +2,32 @@
 
 set -eu
 
-version=$(cat version/version)
-pushd compilation-vars
-  concourse_stemcell_url=$(jq -r .concourse_stemcell_url compilation-vars.json)
-  concourse_stemcell_version=$(jq -r .concourse_stemcell_version compilation-vars.json)
-  director_stemcell_url=$(jq -r .director_stemcell_url compilation-vars.json)
-  director_stemcell_version=$(jq -r .director_stemcell_version compilation-vars.json)
-  concourse_release_url=$(jq -r .concourse_release_url compilation-vars.json)
-  concourse_release_version=$(jq -r .concourse_release_version compilation-vars.json)
-  garden_release_url=$(jq -r .garden_release_url compilation-vars.json)
-  garden_release_version=$(jq -r .garden_release_version compilation-vars.json)
-  director_bosh_release_url=$(jq -r .director_bosh_release_url compilation-vars.json)
-  director_bosh_release_version=$(jq -r .director_bosh_release_version compilation-vars.json)
-  director_bosh_cpi_release_url=$(jq -r .director_bosh_cpi_release_url compilation-vars.json)
-  director_bosh_cpi_release_version=$(jq -r .director_bosh_cpi_release_version compilation-vars.json)
-  director_bpm_release_url=$(jq -r .director_bpm_release_url compilation-vars.json)
-  director_bpm_release_version=$(jq -r .director_bpm_release_version compilation-vars.json)
-  concourse_credhub_release_version=$(jq -r .credhub_release_version compilation-vars.json)
-  concourse_credhub_release_url=$(jq -r .credhub_release_url compilation-vars.json)
-  concourse_grafana_release_version=$(jq -r .grafana_release_version compilation-vars.json)
-  concourse_grafana_release_url=$(jq -r .grafana_release_url compilation-vars.json)
-  concourse_influxdb_release_version=$(jq -r .influxdb_release_version compilation-vars.json)
-  concourse_influxdb_release_url=$(jq -r .influxdb_release_url compilation-vars.json)
-  concourse_riemann_release_version=$(jq -r .riemann_release_version compilation-vars.json)
-  concourse_riemann_release_url=$(jq -r .riemann_release_url compilation-vars.json)
-  concourse_uaa_release_version=$(jq -r .uaa_release_version compilation-vars.json)
-  concourse_uaa_release_url=$(jq -r .uaa_release_url compilation-vars.json)
-  bosh_cli_version=$(jq -r .director_linux_binary_url compilation-vars.json | awk -F- '{print $5}')
-  terraform_version=$(jq -r .terraform_linux_binary_url compilation-vars.json | awk -F- '{print $NF}')
+# version=$(cat version/version)
+pushd resources
+  bin_bosh_cli_version=$(                jq -r '."bosh-cli".linux' director-versions.json)
+  bin_terraform_version=$(               jq -r '.terraform.linux' director-versions.json)
+  deployment_concourse_release_url=$(    jq -r '.[] | select(.value.name? == "concourse") | .value.url' versions.json)
+  deployment_concourse_release_version=$(jq -r '.[] | select(.value.name? == "concourse") | .value.version' versions.json)
+  deployment_credhub_release_url=$(      jq -r '.[] | select(.value.name? == "credhub") | .value.url' versions.json)
+  deployment_credhub_release_version=$(  jq -r '.[] | select(.value.name? == "credhub") | .value.version' versions.json)
+  deployment_garden_release_url=$(       jq -r '.[] | select(.value.name? == "garden-runc") | .value.url' versions.json)
+  deployment_garden_release_version=$(   jq -r '.[] | select(.value.name? == "garden-runc") | .value.version' versions.json)
+  deployment_grafana_release_url=$(      jq -r '.[] | select(.value.name? == "grafana") | .value.url' versions.json)
+  deployment_grafana_release_version=$(  jq -r '.[] | select(.value.name? == "grafana") | .value.version' versions.json)
+  deployment_influxdb_release_url=$(     jq -r '.[] | select(.value.name? == "influxdb") | .value.url' versions.json)
+  deployment_influxdb_release_version=$( jq -r '.[] | select(.value.name? == "influxdb") | .value.version' versions.json)
+  deployment_riemann_release_url=$(      jq -r '.[] | select(.value.name? == "riemann") | .value.url' versions.json)
+  deployment_riemann_release_version=$(  jq -r '.[] | select(.value.name? == "riemann") | .value.version' versions.json)
+  deployment_stemcell_version=$(         jq -r '.[] | select(.path == "/stemcells/alias=trusty/version") | .value' versions.json)
+  deployment_uaa_release_url=$(          jq -r '.[] | select(.value.name? == "uaa") | .value.url' versions.json)
+  deployment_uaa_release_version=$(      jq -r '.[] | select(.value.name? == "uaa") | .value.version' versions.json)
+  director_bosh_cpi_release_url=$(       jq -r .cpi.url director-versions.json)
+  director_bosh_cpi_release_version=$(   jq -r .cpi.version director-versions.json)
+  director_bosh_release_url=$(           jq -r .bosh.url director-versions.json)
+  director_bosh_release_version=$(       jq -r .bosh.version director-versions.json)
+  director_bpm_release_url=$(            jq -r .bpm.url director-versions.json)
+  director_bpm_release_version=$(        jq -r .bpm.version director-versions.json)
+  director_stemcell_version=$(           jq -r .stemcell.url director-versions.json | cut -d= -f2)
 popd
 
 name="concourse-up $version"
@@ -41,20 +39,20 @@ Auto-generated release
 
 Deploys:
 
-- Concourse VM stemcell [bosh-aws-xen-hvm-ubuntu-trusty-go_agent $concourse_stemcell_version]($concourse_stemcell_url)
-- Director stemcell [bosh-aws-xen-hvm-ubuntu-trusty-go_agent $director_stemcell_version]($director_stemcell_url)
-- Concourse [$concourse_release_version]($concourse_release_url)
-- Garden RunC [$garden_release_version]($garden_release_url)
+- Concourse VM stemcell bosh-aws-xen-hvm-ubuntu-trusty-go_agent $deployment_stemcell_version
+- Director stemcell     bosh-aws-xen-hvm-ubuntu-trusty-go_agent $director_stemcell_version
+- Concourse [$deployment_concourse_release_version]($deployment_concourse_release_url)
+- Garden RunC [$deployment_garden_release_version]($deployment_garden_release_url)
 - BOSH [$director_bosh_release_version]($director_bosh_release_url)
 - BOSH AWS CPI [$director_bosh_cpi_release_version]($director_bosh_cpi_release_url)
 - BPM [$director_bpm_release_version]($director_bpm_release_url)
-- Credhub [$concourse_credhub_release_version]($concourse_credhub_release_url)
-- Grafana [$concourse_grafana_release_version]($concourse_grafana_release_url)
-- InfluxDB [$concourse_influxdb_release_version]($concourse_influxdb_release_url)
-- Riemann [$concourse_riemann_release_version]($concourse_riemann_release_url)
-- UAA [$concourse_uaa_release_version]($concourse_uaa_release_url)
-- BOSH CLI $bosh_cli_version
-- Terraform $terraform_version
+- Credhub [$deployment_credhub_release_version]($deployment_credhub_release_url)
+- Grafana [$deployment_grafana_release_version]($deployment_grafana_release_url)
+- InfluxDB [$deployment_influxdb_release_version]($deployment_influxdb_release_url)
+- Riemann [$deployment_riemann_release_version]($deployment_riemann_release_url)
+- UAA [$deployment_uaa_release_version]($deployment_uaa_release_url)
+- BOSH CLI $bin_bosh_cli_version
+- Terraform $bin_terraform_version
 EOF
 
 pushd concourse-up
