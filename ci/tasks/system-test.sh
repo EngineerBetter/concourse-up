eee77fe12f93d2081780f3e07bcdbab32cf41cb3
Paddy Steed <paddy.steed@engineerbetter.com>
2018-07-27 16:49:58 +0100

    add verbose flag to system-test.sh
    Signed-off-by: Sapphire Mason-Brown <sapphire.mason-brown@engineerbetter.com>

--- ci/tasks/system-test.sh
+++ ci/tasks/system-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-set -e
-[ "$VERBOSE" ] && { set -x; export BOSH_LOG_LEVEL=debug; export BOSH_LOG_PATH=bosh.log; }
+set -ex
+[ "$VERBOSE" ] && { export BOSH_LOG_LEVEL=debug; export BOSH_LOG_PATH=bosh.log; }
 if [ -z "$SYSTEM_TEST_ID" ]; then
   SYSTEM_TEST_ID=$RANDOM
 fi
