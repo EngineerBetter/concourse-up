612a522028ca4c470ec9c7ce6ed66a31457e7f65
Panagiotis Xynos <panos@engineerbetter.com>
2018-08-09 10:23:24 +0100

    awk filtering needed some refactoring
    Signed-off-by: Sapphire Mason-Brown <sapphire.mason-brown@engineerbetter.com>

--- ci/tasks/cleanup-tests.sh
+++ ci/tasks/cleanup-tests.sh
@@ -7,6 +7,6 @@ cp binary-linux/concourse-up-linux-amd64 ./cup
 chmod +x ./cup
 
 aws s3 ls \
-| awk -F- '/concourse-up-systest/{print "yes yes | ./cup destroy --region "$8"-"$9"-"$10" "$5"-"$6"-"$7}' \
+| awk -F- '/concourse-up-systest/{print "yes yes | ./cup destroy --region "$7"-"$8"-"$9" "$5"-"$6}' \
 | sort -u \
 | xargs -P 8 -I {} bash -c '{}'
