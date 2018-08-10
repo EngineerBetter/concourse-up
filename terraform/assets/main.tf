7be040965810eb2e757335dff39ff3e5b2e2cb76
Panagiotis Xynos <panos@engineerbetter.com>
2018-08-10 13:05:53 +0100

    remove ssd update while testing
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- terraform/assets/main.tf
+++ terraform/assets/main.tf
@@ -551,7 +551,7 @@ resource "aws_db_instance" "default" {
   vpc_security_group_ids = ["${aws_security_group.rds.id}"]
   db_subnet_group_name   = "${aws_db_subnet_group.default.name}"
   skip_final_snapshot    = true
-  storage_type           = "gp2"
+  # storage_type           = "gp2"
   lifecycle {
     ignore_changes = ["allocated_storage"]
   }
