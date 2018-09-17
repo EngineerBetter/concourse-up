beb95def1f304bf060ac74f91134011b6cd799ab
Paddy Steed <paddy.steed@engineerbetter.com>
2018-09-17 12:43:17 +0100

    make user IP check more robust
    Signed-off-by: Panagiotis Xynos <panos@engineerbetter.com>

--- util/ip.go
+++ util/ip.go
@@ -1,22 +1,32 @@
 package util
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 // FindUserIP gets the user's public IP by querying whatismyip.akamai.com
 func FindUserIP() (string, error) {
-	resp, err := http.Get("http://whatismyip.akamai.com")
-	if err != nil {
-		return "", err
-	}
+	const retries = 10
+	for i := 0; i < retries; i++ {
+		resp, err := http.Get("http://whatismyip.akamai.com")
+		if err != nil {
+			return "", err
+		}
 
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
+		bytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
 
-	return strings.TrimSpace(string(bytes)), nil
+		ip := strings.TrimSpace(string(bytes))
+		if ip != "" {
+			return ip, nil
+		}
+		time.Sleep(time.Duration(i) * time.Second)
+	}
+	return "", errors.New("timed out getting user IP")
 }
