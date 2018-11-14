4ea7e81399a6a53655456d4f255f5357105b32cc
Panagiotis Xynos <panos@engineerbetter.com>
2018-11-14 12:10:48 +0000

    AWSProvider DeleteVolume() is being tested (integration test)
    Signed-off-by: Irbe Krumina <irbe.krumina@engineerbetter.com>

---
+++ iaas/aws_integration_test.go
@@ -0,0 +1,63 @@
+// +build integration
+
+package iaas
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func TestAWSProvider_DeleteVolumes(t *testing.T) {
+	region := "us-east-1"
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	svc := ec2.New(sess)
+	input := &ec2.CreateVolumeInput{
+		AvailabilityZone: aws.String("us-east-1a"),
+		Size:             aws.Int64(1),
+		VolumeType:       aws.String("gp2"),
+	}
+	result, err := svc.CreateVolume(input)
+	if err != nil {
+		t.Error("Cannot create a volume")
+	}
+
+	type args struct {
+		volumes      []string
+		deleteVolume func(ec2Client IEC2, volumeID *string) error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "find all created volumes for deletetion",
+			args: args{
+				volumes: []string{*result.VolumeId},
+				deleteVolume: func(ec2Client IEC2, volumeID *string) error {
+					fmt.Printf("volume %s is about to be deleted\n", *volumeID)
+					return nil
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			a := &AWSProvider{
+				sess: sess,
+			}
+
+			if err := a.DeleteVolumes(tt.args.volumes, DeleteVolume); (err != nil) != tt.wantErr {
+				t.Errorf("AWSProvider.DeleteVolumes() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
