7a20327e3a02ded17d8250bd2bb49542700597e4
Jessica Stenning <jessica.stenning@engineerbetter.com>
2018-11-23 15:51:24 +0000

    erroneous zone arguments should be trapped while redeploying
    Signed-off-by: Panagiotis Xynos <panos@engineerbetter.com>

--- config/client.go
+++ config/client.go
@@ -180,6 +180,10 @@ func (client *Client) LoadOrCreate(deployArgs *DeployArgs) (Config, bool, bool, 
 	}
 
 	if deployArgs.ZoneIsSet {
+		// This is a safeguard for a redeployment where zone does not belong to the region where the original deployment has happened
+		if !newConfigCreated && !strings.Contains(deployArgs.Zone, config.AvailabilityZone) {
+			return Config{}, false, false, fmt.Errorf("Zone %s does not belong to region %s", deployArgs.Zone, config.AvailabilityZone)
+		}
 		config.AvailabilityZone = deployArgs.Zone
 	}
 	if newConfigCreated || deployArgs.WorkerCountIsSet {
