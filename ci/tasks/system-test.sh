845d630034dd2d7d66ed60bad785d87551dcd8b9
Jessica Stenning <jessica.stenning@engineerbetter.com>
2018-12-05 09:43:42 +0000

    refactor system-test to test GCP
    Signed-off-by: Irbe Krumina <irbe.krumina@engineerbetter.com>

--- ci/tasks/system-test.sh
+++ ci/tasks/system-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+: "${IAAS:=AWS}"
+
 set -ex
 [ "$VERBOSE" ] && { export BOSH_LOG_LEVEL=debug; export BOSH_LOG_PATH=bosh.log; }
 if [ -z "$SYSTEM_TEST_ID" ]; then
@@ -9,10 +11,21 @@ deployment="systest-$SYSTEM_TEST_ID"
 set -u
 
 assertDbCorrect() {
+  if [ "$IAAS" = "AWS" ]; then
   rds_instance_class=$(aws --region eu-west-1 rds describe-db-instances | jq -r ".DBInstances[] | select(.DBSubnetGroup.DBSubnetGroupName==\"concourse-up-$deployment\") | .DBInstanceClass")
-  if [ "$rds_instance_class" != "db.t2.small" ]; then
-    echo "Unexpected DB instance class: $rds_instance_class"
-    exit 1
+    if [ "$rds_instance_class" != "db.t2.small" ]; then
+      echo "Unexpected DB instance class: $rds_instance_class"
+      exit 1
+    fi
+  elif [ "$IAAS" = "GCP" ]; then
+    rds_instance_tier=$(gcloud sql instances list --filter="labels.deployment:concourse-up-$deployment" --format=json | jq '.[0].settings.tier')
+    if [ "$rds_instance_tier" != "db-f1-micro" ]; then
+      echo "Unexpected DB instance tier: $rds_instance_tier"
+      exit 1
+    else
+      echo "Unknown iaas: $IAAS"
+      exit 1
+    fi
   fi
 }
 
