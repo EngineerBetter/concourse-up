b3ec5f289c8b3496de00fe7032278f8057314ec1
Jessica Stenning <jessica.stenning@engineerbetter.com>
2019-01-07 11:38:59 +0000

    Zone needs an argument
    Signed-off-by: Irbe Krumina <irbe.krumina@engineerbetter.com>

--- iaas/aws_integration_test.go
+++ iaas/aws_integration_test.go
@@ -91,7 +91,7 @@ func TestAWSProvider_Zone(t *testing.T) {
 				sess:       sess,
 				workerType: tt.fields.workerType,
 			}
-			if got := a.Zone(); got != tt.want {
+			if got := a.Zone(""); got != tt.want {
 				t.Errorf("AWSProvider.Zone() = %v, want %v", got, tt.want)
 			}
 		})
