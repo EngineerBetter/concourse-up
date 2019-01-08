384018b80d528236e4f412a7d7ef579291d2d2ef
Irbe Krumina <irbe.krumina@engineerbetter.com>
2019-01-08 15:51:25 +0000

    implements systest destroy for GCP

--- ci/tasks/system-test-options-and-destroy.sh
+++ ci/tasks/system-test-options-and-destroy.sh
@@ -20,22 +20,40 @@ source concourse-up/ci/tasks/lib/github-auth.sh
 source concourse-up/ci/tasks/lib/tags.sh
 # shellcheck disable=SC1091
 source concourse-up/ci/tasks/lib/credhub.sh
+
 # shellcheck disable=SC1091
-source concourse-up/ci/tasks/lib/destroy.sh
+[ "$IAAS" = "AWS" ] && { source concourse-up/ci/tasks/lib/destroy.sh; }
+
+# shellcheck disable=SC1091
+[ "$IAAS" = "GCP" ] && { source concourse-up/ci/tasks/lib/gcp-destroy.sh; }
+
+# If we're testing GCP, we need credentials to be available as a file
+[ "$IAAS" = "GCP" ] && { setGoogleCreds; }
 
 
 cp "$BINARY_PATH" ./cup
 chmod +x ./cup
 
-addGitHubFlagsToArgs
-addTagsFlagsToArgs
-./cup deploy "${args[@]}" $deployment
-assertTagsSet
-assertGitHubAuthConfigured
-assertPipelinesCanReadFromCredhub
-sleep 60
-recordDeployedState
-echo "non-interactive destroy"
-./cup --non-interactive destroy --region us-east-1 "$deployment"
+# Temporary fix whilst we haven't implemented the rest for GCP
+if [ "$IAAS" = "AWS" ]
+then
+    addGitHubFlagsToArgs
+    addTagsFlagsToArgs
+    ./cup deploy "${args[@]}" $deployment
+    assertTagsSet
+    assertGitHubAuthConfigured
+    assertPipelinesCanReadFromCredhub
+    sleep 60
+    recordDeployedState
+    echo "non-interactive destroy"
+    ./cup --non-interactive destroy --region us-east-1 "$deployment"
+elif [ "$IAAS" = "GCP" ]
+then
+    ./cup deploy $deployment -iaas gcp
+    sleep 60
+    recordDeployedState
+    echo "non-interactive destroy"
+    ./cup --non-interactive destroy "$deployment" -iaas gcp
+fi
 sleep 180
 assertEverythingDeleted
