6158188bbcb0ebe36322e7116e4d9b01757a78e9
Irbe Krumina <irbe.krumina@engineerbetter.com>
2019-01-09 10:18:07 +0000

    shellcheck is happy
    Signed-off-by: Jessica Stenning <jessica.stenning@engineerbetter.com>

--- ci/tasks/lib/gcp-destroy.sh
+++ ci/tasks/lib/gcp-destroy.sh
@@ -2,9 +2,9 @@
 
 function recordDeployedState() {
     echo "Record ids of Concourse componenents"
-    vpc_name=$(gcloud compute networks list --filter="name ~ concourse-up-$deployment" --format=json | jq -r '.[0].name') 
+    vpc_name=$(gcloud compute networks list --filter="name ~ concourse-up-$deployment" --format=json | jq -r '.[0].name')
     instances=$(gcloud compute instances list --filter="networkInterfaces[0].network:$vpc_name" --format=json)
-    
+
     existing_volumes=$(echo "$instances" | tr -d "[:cntrl:]" | jq -r '[.[].disks[].source]')
     echo "Get terraform state from bucket"
     config_bucket="concourse-up-$deployment-europe-west1-config"
@@ -12,12 +12,13 @@ function recordDeployedState() {
 
     echo "Record name of db instance"
     cloud_sql_instance_name=$(terraform output -state=default.tfstate -json | jq -r '.db_name.value')
-    
+
 }
 
 function assertEverythingDeleted() {
     echo "Check that volumes have been deleted"
     all_volumes=$(gcloud compute instances list --format=json | jq '[.[].disks[].source]')
+    # shellcheck disable=SC2016
     volumes_count=$(echo "$existing_volumes $all_volumes" | jq --slurp '[.[0][] as $x | .[1][] | select($x == .)] | length')
     echo "Volumes still remaining after deletion: $volumes_count"
     [ "$volumes_count" -eq 0 ]
