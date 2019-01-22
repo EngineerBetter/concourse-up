49ddc8428e98130e0151a399559562d1dff05254
Colin Simmons <colin.simmons@engineerbetter.com>
2019-01-22 10:52:51 +0000

    run ginkgo as well in local test script
    Signed-off-by: Panagiotis Xynos <panos@engineerbetter.com>

--- run_tests_local.sh
+++ run_tests_local.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#shellcheck disable=SC2016
 
 set -eu
 
-docker run -it -v "${PWD}:/mnt/concourse-up" -v "${PWD}/../concourse-up-ops:/mnt/concourse-up-ops" engineerbetter/pcf-ops bash -c "cp -r /mnt/concourse-up* .; ./concourse-up/ci/tasks/lint.sh"
+docker run -it \
+    -v "${PWD}:/mnt/concourse-up" \
+    -v "${PWD}/../concourse-up-ops:/mnt/concourse-up-ops" \
+    engineerbetter/pcf-ops \
+    bash -c \
+    'cp -r /mnt/concourse-up* .; ./concourse-up/ci/tasks/lint.sh && cd ${GOPATH}/src/github.com/EngineerBetter/concourse-up && ginkgo -r'
