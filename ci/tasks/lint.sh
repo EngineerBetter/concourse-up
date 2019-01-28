843e18bbe0ba8c6bd5e70ff413b92b1609736480
Colin Simmons <colin.simmons@engineerbetter.com>
2019-01-28 11:11:29 +0000

    make shell scripts fail if cds do
    Signed-off-by: Daniel Jones <daniel.jones@engineerbetter.com>

--- ci/tasks/lint.sh
+++ ci/tasks/lint.sh
@@ -7,7 +7,7 @@ mkdir -p "$GOPATH/src/github.com/EngineerBetter/concourse-up"
 mkdir -p "$GOPATH/src/github.com/EngineerBetter/concourse-up-ops"
 mv concourse-up/* "$GOPATH/src/github.com/EngineerBetter/concourse-up"
 mv concourse-up-ops/* "$GOPATH/src/github.com/EngineerBetter/concourse-up-ops"
-cd "$GOPATH/src/github.com/EngineerBetter/concourse-up"
+cd "$GOPATH/src/github.com/EngineerBetter/concourse-up" || exit 1
 
 go get -u github.com/mattn/go-bindata/...
 go generate github.com/EngineerBetter/concourse-up/...
--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -eu
+# shellcheck disable=SC1091
+source concourse-up/ci/tasks/lib/set-flags.sh
 
 mkdir -p "$GOPATH/src/github.com/EngineerBetter/concourse-up"
 mkdir -p "$GOPATH/src/github.com/EngineerBetter/concourse-up-ops"
 mv concourse-up/* "$GOPATH/src/github.com/EngineerBetter/concourse-up"
 mv concourse-up-ops/* "$GOPATH/src/github.com/EngineerBetter/concourse-up-ops"
-cd "$GOPATH/src/github.com/EngineerBetter/concourse-up"
+cd "$GOPATH/src/github.com/EngineerBetter/concourse-up" || exit 1
 
 go get -u github.com/mattn/go-bindata/...
 go generate github.com/EngineerBetter/concourse-up/...
