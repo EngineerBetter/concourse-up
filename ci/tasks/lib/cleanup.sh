f8c617f8438c244f52ede8baa9ad66df39c2b251
Colin Simmons <colin.simmons@engineerbetter.com>
2019-01-29 16:51:16 +0000

    don't fail in trap cleanup

--- ci/tasks/lib/cleanup.sh
+++ ci/tasks/lib/cleanup.sh
@@ -3,13 +3,17 @@
 # cleans up a deployment in the default region
 function defaultCleanup() {
     status=$?
+    set +e
     ./cup --non-interactive destroy "$deployment"
+    set -e
     exit $status
 }
 
 # cleans up a deployment, takes region as an argument
 function customCleanup() {
   status=$?
+  set +e
   ./cup --non-interactive destroy "$deployment" --region "$region"
+  set -e
   exit $status
 }
