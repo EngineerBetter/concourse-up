3d16658d117be16c87ba5f3bb5ae8a2443a00f5c
Daniel Jones <daniel.jones@engineerbetter.com>
2019-01-30 14:50:20 +0000

    parameterise CI bucket name

--- ci/tf/ci-bucket.tf
+++ ci/tf/ci-bucket.tf
@@ -1,11 +1,14 @@
 variable "region" {}
+variable "bucket-name" {
+  default = "concourse-up-ci-artifacts"
+}
 
 provider "aws" {
   region = "${var.region}"
 }
 
 resource "aws_s3_bucket" "ci" {
-  bucket        = "concourse-up-ci-artifacts"
+  bucket        = "${var.bucket-name}"
   acl           = "private"
   versioning {
     enabled = true
