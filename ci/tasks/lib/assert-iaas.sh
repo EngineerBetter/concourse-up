ec76c552a73a55158df2a79c678d007b08628b31
Daniel Jones <daniel.jones@engineerbetter.com>
2019-02-07 15:06:09 +0000

    appease shellcheck

--- ci/tasks/lib/assert-iaas.sh
+++ ci/tasks/lib/assert-iaas.sh
@@ -3,11 +3,13 @@
 if [ "$IAAS" = "AWS" ]; then
     [[ -n "$AWS_ACCESS_KEY_ID" ]]
     [[ -n "$AWS_SECRET_ACCESS_KEY" ]]
+    # shellcheck disable=SC2034
     region=eu-west-1
 elif [ "$IAAS" = "GCP" ]; then
     [[ -n "$GOOGLE_APPLICATION_CREDENTIALS_CONTENTS" ]]
     # shellcheck disable=SC1091
     source concourse-up/ci/tasks/lib/gcreds.sh
     setGoogleCreds
+    # shellcheck disable=SC2034
     region=europe-west1
 fi
