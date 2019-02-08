c313f9d1d3d5b72e6d6ea7a6c21ec71983c606c9
Daniel Jones <daniel.jones@engineerbetter.com>
2019-02-08 14:22:05 +0000

    make system test cleanup work for AWS after naming convention change

--- ci/tasks/cleanup-tests.rb
+++ ci/tasks/cleanup-tests.rb
@@ -3,35 +3,44 @@
 `cp binary-linux/concourse-up-linux-amd64 ./cup`
 `chmod +x ./cup`
 
+system_tests_name_prefix = "concourse-up-ts"
+
 non_empty = []
 
-buckets = `aws s3 ls | grep -E 'concourse-up-systest' | awk '{print $3}'`
+buckets = `aws s3 ls | grep -E '#{system_tests_name_prefix}' | awk '{print $3}'`
 
 if buckets.empty?
   puts 'No residual systest buckets found'
   exit(0)
 end
 
+Orphan = Struct.new(:bucket, :deployment, :project, :region)
+
 buckets.each_line do |bucket|
   bucket = bucket.strip
   deployment = bucket.split('-')[0..4].join('-')
+  project = bucket.split('-')[2..4].join('-')
   region = bucket.split('-')[5..7].join('-')
+  orphan = Orphan.new(bucket, deployment, project, region)
 
-  aws_region_cmd = "aws --region #{region}"
+  puts "Processing #{orphan}"
 
-  `aws s3 ls s3://#{bucket} > /dev/null`
+  aws_region_cmd = "aws --region #{orphan.region}"
+
+  `aws s3 ls s3://#{orphan.bucket} > /dev/null`
   if $?.success?
-    contents = `aws s3 ls s3://#{bucket}`
+    contents = `aws s3 ls s3://#{orphan.bucket}`
     if contents.include?('terraform.tfstate') && contents.include?('director-state.json')
-      non_empty.push(bucket)
+      non_empty.push(orphan)
     else
-      puts "#{bucket} is missing key files"
+      puts "#{orphan.bucket} is missing key files"
       puts 'Attempting to delete VPC'
       puts '==================================================================='
-      puts "MANUAL CLEANUP MAY BE REQUIRED FOR #{bucket}"
+      puts "MANUAL CLEANUP MAY BE REQUIRED FOR #{orphan.bucket}"
       puts '==================================================================='
-      vpc_id = `#{aws_region_cmd} ec2 describe-vpcs --filters 'Name=tag:Name,Values=#{deployment}' | jq -r '.Vpcs[0].VpcId'`
-      if vpc_id
+
+      vpc_id = `#{aws_region_cmd} ec2 describe-vpcs --filters 'Name=tag:Name,Values=#{orphan.project}' | jq -r '.Vpcs[0].VpcId'`
+      unless vpc_id.empty? || vpc_id.include?('null')
         rds_instance = `#{aws_region_cmd} rds describe-db-instances | jq -r '.DBInstances[] | select(.DBSubnetGroup.VpcId == "#{vpc_id}") | .DBInstanceIdentifier'`
         if rds_instance
           `#{aws_region_cmd} rds delete-db-instance --db-instance-identifier "#{rds_instance}" --skip-final-snapshot true`
@@ -39,34 +48,28 @@ buckets.each_line do |bucket|
         end
         `#{aws_region_cmd} ec2 delete-vpc --vpc-id #{vpc_id}`
       else
-        puts "vpc for #{deployment} not found"
+        puts "vpc for #{orphan.deployment} not found"
       end
-      `aws s3 rb s3://#{bucket} --force`
+      `aws s3 rb s3://#{orphan.bucket} --force`
+      puts "Deleted bucket #{orphan.bucket}"
     end
   else
-    printf "#{bucket} doesn't really exist\n"
+    printf "#{orphan.bucket} doesn't really exist\n"
   end
 end
 
 exit(0) if non_empty.empty?
 
-non_empty.each do |bucket|
-  if /^concourse-up-systest-[0-9]+/ =~ "#{bucket}"
-    deployment = bucket.split('-')[2..3].join('-')
-    region = bucket.split('-')[4..6].join('-')
-    str = sprintf("yes yes | ./cup destroy --region %s %s", region, deployment)
-    `echo #{str} >> to_delete`
-  elsif /^concourse-up-systest-[a-zA-Z]+-[0-9]+/ =~ "#{bucket}"
-    deployment = bucket.split('-')[2..4].join('-')
-    region = bucket.split('-')[5..7].join('-')
-    str = sprintf("yes yes | ./cup destroy --region %s %s", region, deployment)
-    `echo #{str} >> to_delete`
-  else
-    puts "Unexpected bucket format #{bucket} -- skipping"
-  end
+non_empty.each do |orphan|
+  command = "./cup destroy --region #{orphan.region} #{orphan.project}"
+  puts "Attempting to run #{command}"
+  str = sprintf("yes yes | ./cup destroy --region %s %s", orphan.region, orphan.project)
+  `echo '#{str}' >> to_delete`
 end
 
-`sort -u to_delete \
+# Do deletes in Bash so we can see STDOUT as it happens without needing any Gems
+`set -x && \
+ sort -u to_delete \
  | xargs -P 8 -I {} bash -c '{}'`
 
 `rm -f to_delete`
