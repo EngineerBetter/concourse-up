233a52600949d0bd1f5f19d0979ddf64d5212c43
Colin Simmons <colin.simmons@engineerbetter.com>
2019-02-11 15:30:25 +0000

    allow cleanup of GCP things that live in a region or zone
    Signed-off-by: Daniel Jones <daniel.jones@engineerbetter.com>

--- ci/tasks/ruby-lib/gcp.rb
+++ ci/tasks/ruby-lib/gcp.rb
@@ -42,22 +42,24 @@ class GCP
       bucket.delete_empty
     else
       found_components = 0
-      found_components += delete_regional_components('compute instances', "labels.deployment:bosh AND labels.concourse-up-project:#{orphan.project}", 'name', orphan.region)
-      found_components += delete_regional_components('compute instances', "labels.concourse-up-project:#{orphan.project}", 'name', orphan.region)
-      found_components += delete_regional_components('compute instances', "name:'#{orphan.deployment}-nat-instance'", 'name', orphan.region)
+      found_components += delete_components('compute instances', "labels.deployment:bosh AND labels.concourse-up-project:#{orphan.project}", 'name')
+      found_components += delete_components('compute instances', "labels.concourse-up-project:#{orphan.project}", 'name')
+      found_components += delete_components('compute instances', "name:'#{orphan.deployment}-nat-instance'", 'name')
       found_components += delete_regional_components('compute networks subnets', "name~'.*#{orphan.project}.*'", 'name', orphan.region)
       found_components += delete_components('compute routes', "network~'.*#{orphan.deployment}.*'", 'name')
       found_components += delete_components('compute firewall-rules', "name~'.*#{orphan.project}.*'", 'name')
       found_components += delete_components('compute networks', "name~'.*#{orphan.project}.*'", 'name')
-      found_components += delete_components('sql instances', "name~'.*#{orphan.project}.*'", 'name')
+      found_components += delete_components('sql instances', "labels.deployment:#{orphan.deployment}", 'name')
       found_components += delete_components('iam service-accounts', "email~'.*#{orphan.project}.*'", 'uniqueId')
-      found_components += delete_components('compute addresses', "name~'.*#{orphan.project}.*'", 'name')
+      found_components += delete_regional_components('compute addresses', "name~'.*#{orphan.project}.*'", 'name', orphan.region)
 
       # Only delete the bucket if there are no orphaned components. This will never be true on the first run.
       # We need to do this, rather than detecting failures to delete, as we don't know _why_ the deployment
       # failed and so half of the components could have been missing to start with.
       if found_components == 0
         bucket.delete
+      else
+        puts "Not deleting bucket #{bucket.name} as components were found on this run. It will be deleted once no components are found."
       end
     end
   end
@@ -65,15 +67,33 @@ class GCP
   private
 
   def delete_components(component_type, filter, identifier_key, region_flag = "")
-    componentsJson = `gcloud --format json #{component_type} list --filter="#{filter}"`
-    components = JSON.parse(componentsJson)
-    componentIdentifiers = components.map { |component| component.fetch("#{identifier_key}") }
-    componentIdentifiers.each { |componentIdentifier| `gcloud --quiet #{region_flag} #{component_type} delete #{componentIdentifier}` }
+    components_json = `gcloud --format json #{component_type} list --filter="#{filter}"`
+    components = JSON.parse(components_json)
+    component_ids_and_zones = components.map do |component|
+      id_and_zone = {}
+      id_and_zone[:id] = component.fetch("#{identifier_key}")
+
+      if component.key?('zone')
+        id_and_zone[:zone] = component["zone"]
+      end
+
+      id_and_zone
+    end
+
+    component_ids_and_zones.each do |id_and_zone|
+      zoneFlag = ''
+
+      if id_and_zone.key?(:zone)
+        zone_flag = "--zone #{id_and_zone.fetch(:zone)}"
+      end
+
+      `gcloud --quiet #{component_type} delete #{id_and_zone.fetch(:id)} #{region_flag} #{zone_flag}`
+    end
     components.size
   end
 
-  def delete_regional_component(component_type, filter, identifier_key, region)
-    delete_components(component_type, filter, identifier_key, "--region=#{region}")
+  def delete_regional_components(component_type, filter, identifier_key, region)
+    delete_components(component_type, filter, identifier_key, "--region #{region}")
   end
 end
 
