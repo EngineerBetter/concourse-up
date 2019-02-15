fcef8eedd90b9769acbf94755b9dfc8868e86e4c
Colin Simmons <colin.simmons@engineerbetter.com>
2019-02-15 09:40:50 +0000

    print gcp dependency versions in release notes
    Signed-off-by: Irbe Krumina <irbe.krumina@engineerbetter.com>

--- ci/tasks/write-release-vars.sh
+++ ci/tasks/write-release-vars.sh
@@ -34,30 +34,30 @@ pushd concourse-up-ops
   director_bpm_release_version=$(        jq -r .bpm.version director-versions-aws.json)
   director_stemcell_version=$(           jq -r .stemcell.url director-versions-aws.json | cut -d= -f2)
 
-  # bin_bosh_cli_version_gcp=$(                jq -r '."bosh-cli".linux' director-versions-gcp.json)
-  # bin_terraform_version_gcp=$(               jq -r '.terraform.linux' director-versions-gcp.json)
-  # deployment_concourse_release_url_gcp=$(    jq -r '.[] | select(.value.name? == "concourse") | .value.url' ops/versions-gcp.json)
-  # deployment_concourse_release_version_gcp=$(jq -r '.[] | select(.value.name? == "concourse") | .value.version' ops/versions-gcp.json)
-  # deployment_credhub_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "credhub") | .value.url' ops/versions-gcp.json)
-  # deployment_credhub_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "credhub") | .value.version' ops/versions-gcp.json)
-  # deployment_garden_release_url_gcp=$(       jq -r '.[] | select(.value.name? == "garden-runc") | .value.url' ops/versions-gcp.json)
-  # deployment_garden_release_version_gcp=$(   jq -r '.[] | select(.value.name? == "garden-runc") | .value.version' ops/versions-gcp.json)
-  # deployment_grafana_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "grafana") | .value.url' ops/versions-gcp.json)
-  # deployment_grafana_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "grafana") | .value.version' ops/versions-gcp.json)
-  # deployment_influxdb_release_url_gcp=$(     jq -r '.[] | select(.value.name? == "influxdb") | .value.url' ops/versions-gcp.json)
-  # deployment_influxdb_release_version_gcp=$( jq -r '.[] | select(.value.name? == "influxdb") | .value.version' ops/versions-gcp.json)
-  # deployment_riemann_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "riemann") | .value.url' ops/versions-gcp.json)
-  # deployment_riemann_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "riemann") | .value.version' ops/versions-gcp.json)
-  # deployment_stemcell_version_gcp=$(         jq -r '.[] | select(.path == "/stemcells/alias=xenial/version") | .value' ops/versions-gcp.json)
-  # deployment_uaa_release_url_gcp=$(          jq -r '.[] | select(.value.name? == "uaa") | .value.url' ops/versions-gcp.json)
-  # deployment_uaa_release_version_gcp=$(      jq -r '.[] | select(.value.name? == "uaa") | .value.version' ops/versions-gcp.json)
-  # director_bosh_cpi_release_url_gcp=$(       jq -r .cpi.url director-versions-gcp.json)
-  # director_bosh_cpi_release_version_gcp=$(   jq -r .cpi.version director-versions-gcp.json)
-  # director_bosh_release_url_gcp=$(           jq -r .bosh.url director-versions-gcp.json)
-  # director_bosh_release_version_gcp=$(       jq -r .bosh.version director-versions-gcp.json)
-  # director_bpm_release_url_gcp=$(            jq -r .bpm.url director-versions-gcp.json)
-  # director_bpm_release_version_gcp=$(        jq -r .bpm.version director-versions-gcp.json)
-  # director_stemcell_version_gcp=$(           jq -r .stemcell.url director-versions-gcp.json | cut -d= -f2)
+  bin_bosh_cli_version_gcp=$(                jq -r '."bosh-cli".linux' director-versions-gcp.json)
+  bin_terraform_version_gcp=$(               jq -r '.terraform.linux' director-versions-gcp.json)
+  deployment_concourse_release_url_gcp=$(    jq -r '.[] | select(.value.name? == "concourse") | .value.url' ops/versions-gcp.json)
+  deployment_concourse_release_version_gcp=$(jq -r '.[] | select(.value.name? == "concourse") | .value.version' ops/versions-gcp.json)
+  deployment_credhub_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "credhub") | .value.url' ops/versions-gcp.json)
+  deployment_credhub_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "credhub") | .value.version' ops/versions-gcp.json)
+  deployment_garden_release_url_gcp=$(       jq -r '.[] | select(.value.name? == "garden-runc") | .value.url' ops/versions-gcp.json)
+  deployment_garden_release_version_gcp=$(   jq -r '.[] | select(.value.name? == "garden-runc") | .value.version' ops/versions-gcp.json)
+  deployment_grafana_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "grafana") | .value.url' ops/versions-gcp.json)
+  deployment_grafana_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "grafana") | .value.version' ops/versions-gcp.json)
+  deployment_influxdb_release_url_gcp=$(     jq -r '.[] | select(.value.name? == "influxdb") | .value.url' ops/versions-gcp.json)
+  deployment_influxdb_release_version_gcp=$( jq -r '.[] | select(.value.name? == "influxdb") | .value.version' ops/versions-gcp.json)
+  deployment_riemann_release_url_gcp=$(      jq -r '.[] | select(.value.name? == "riemann") | .value.url' ops/versions-gcp.json)
+  deployment_riemann_release_version_gcp=$(  jq -r '.[] | select(.value.name? == "riemann") | .value.version' ops/versions-gcp.json)
+  deployment_stemcell_version_gcp=$(         jq -r '.[] | select(.path == "/stemcells/alias=xenial/version") | .value' ops/versions-gcp.json)
+  deployment_uaa_release_url_gcp=$(          jq -r '.[] | select(.value.name? == "uaa") | .value.url' ops/versions-gcp.json)
+  deployment_uaa_release_version_gcp=$(      jq -r '.[] | select(.value.name? == "uaa") | .value.version' ops/versions-gcp.json)
+  director_bosh_cpi_release_url_gcp=$(       jq -r .cpi.url director-versions-gcp.json)
+  director_bosh_cpi_release_version_gcp=$(   jq -r .cpi.version director-versions-gcp.json)
+  director_bosh_release_url_gcp=$(           jq -r .bosh.url director-versions-gcp.json)
+  director_bosh_release_version_gcp=$(       jq -r .bosh.version director-versions-gcp.json)
+  director_bpm_release_url_gcp=$(            jq -r .bpm.url director-versions-gcp.json)
+  director_bpm_release_version_gcp=$(        jq -r .bpm.version director-versions-gcp.json)
+  director_stemcell_version_gcp=$(           jq -r .stemcell.url director-versions-gcp.json | cut -d= -f2)
 popd
 
 pushd ops-version
@@ -67,25 +67,6 @@ popd
 name="concourse-up $version"
 
 echo "$name" > release-vars/name
-
-# Add the following to the release body once GCP feature is announced.
-
-# **GCP**
-
-# - Concourse VM stemcell bosh-google-kvm-ubuntu-xenial-go_agent $deployment_stemcell_version_gcp
-# - Director stemcell     bosh-google-kvm-ubuntu-xenial-go_agent $director_stemcell_version_gcp
-# - Concourse [$deployment_concourse_release_version_gcp]($deployment_concourse_release_url_gcp)
-# - Garden RunC [$deployment_garden_release_version_gcp]($deployment_garden_release_url_gcp)
-# - BOSH [$director_bosh_release_version_gcp]($director_bosh_release_url_gcp)
-# - BOSH AWS CPI [$director_bosh_cpi_release_version_gcp]($director_bosh_cpi_release_url_gcp)
-# - BPM [$director_bpm_release_version_gcp]($director_bpm_release_url_gcp)
-# - Credhub [$deployment_credhub_release_version_gcp]($deployment_credhub_release_url_gcp)
-# - Grafana [$deployment_grafana_release_version_gcp]($deployment_grafana_release_url_gcp)
-# - InfluxDB [$deployment_influxdb_release_version_gcp]($deployment_influxdb_release_url_gcp)
-# - Riemann [$deployment_riemann_release_version_gcp]($deployment_riemann_release_url_gcp)
-# - UAA [$deployment_uaa_release_version_gcp]($deployment_uaa_release_url_gcp)
-# - BOSH CLI $bin_bosh_cli_version_gcp
-# - Terraform $bin_terraform_version_gcp
 
 cat << EOF > release-vars/body
 
@@ -110,8 +91,22 @@ Deploys:
 - BOSH CLI $bin_bosh_cli_version
 - Terraform $bin_terraform_version
 
->Note to build locally you will need to clone [concourse-up-ops](https://github.com/EngineerBetter/concourse-up-ops/tree/$ops_version) (version $ops_version) to the same level as concourse-up to get the required manifests and ops files.
-EOF
+**GCP**
+
+- Concourse VM stemcell bosh-google-kvm-ubuntu-xenial-go_agent $deployment_stemcell_version_gcp
+- Director stemcell     bosh-google-kvm-ubuntu-xenial-go_agent $director_stemcell_version_gcp
+- Concourse [$deployment_concourse_release_version_gcp]($deployment_concourse_release_url_gcp)
+- Garden RunC [$deployment_garden_release_version_gcp]($deployment_garden_release_url_gcp)
+- BOSH [$director_bosh_release_version_gcp]($director_bosh_release_url_gcp)
+- BOSH AWS CPI [$director_bosh_cpi_release_version_gcp]($director_bosh_cpi_release_url_gcp)
+- BPM [$director_bpm_release_version_gcp]($director_bpm_release_url_gcp)
+- Credhub [$deployment_credhub_release_version_gcp]($deployment_credhub_release_url_gcp)
+- Grafana [$deployment_grafana_release_version_gcp]($deployment_grafana_release_url_gcp)
+- InfluxDB [$deployment_influxdb_release_version_gcp]($deployment_influxdb_release_url_gcp)
+- Riemann [$deployment_riemann_release_version_gcp]($deployment_riemann_release_url_gcp)
+- UAA [$deployment_uaa_release_version_gcp]($deployment_uaa_release_url_gcp)
+- BOSH CLI $bin_bosh_cli_version_gcp
+- Terraform $bin_terraform_version_gcp
 
 cat <<EOF > release-vars/slackmsg
 <!channel> Concourse Up $(cat version/version) published to Github
